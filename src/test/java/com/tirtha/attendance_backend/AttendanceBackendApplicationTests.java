436f14eca7ab2d8c7c7286e29f3d9bb5b7fa90a9
Tirtha-bit <tirthadeb88@gmail.com>
2025-12-18 22:47:03 +0530

    Day 1: Spring Boot setup with health endpoint

---
+++ src/test/java/com/tirtha/attendance_backend/AttendanceBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tirtha.attendance_backend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceBackendApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
