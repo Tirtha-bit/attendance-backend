410997b6fbe26328e1a221775c842e484caa7a57
Tirtha-bit <tirthadeb88@gmail.com>
2025-12-24 23:59:56 +0530

    commit

--- src/main/java/com/tirtha/attendance_backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/tirtha/attendance_backend/service/impl/UserServiceImpl.java
@@ -5,32 +5,36 @@ import com.tirtha.attendance_backend.entity.User;
 import com.tirtha.attendance_backend.exception.DuplicateResourceException;
 import com.tirtha.attendance_backend.repository.UserRepository;
 import com.tirtha.attendance_backend.service.UserService;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public UserServiceImpl(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+           this.userRepository = userRepository;
+           this.passwordEncoder = passwordEncoder;
+}
 
     @Override
-    public User createUser(UserCreateRequest request) {
+public User createUser(UserCreateRequest request) {
 
-        // ✅ STEP 1: duplicate email check (BUSINESS RULE)
-        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
-            throw new DuplicateResourceException("Email already exists");
-        }
+    User user = new User();
+    user.setEmail(request.getEmail());
 
-        // ✅ STEP 2: create user entity
-        User user = new User();
-        user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword()); // hashing later (Day 5)
-        user.setRole(request.getRole());
+    // 🔥 HASH PASSWORD HERE
+    user.setPassword(
+        passwordEncoder.encode(request.getPassword())
+    );
 
-        // ✅ STEP 3: save user
-        return userRepository.save(user);
+       user.setRole(request.getRole());
+
+       return userRepository.save(user);
     }
+
 }
