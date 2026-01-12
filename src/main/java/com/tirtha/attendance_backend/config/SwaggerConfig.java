c00a8fb1995699f2ab2a88513373f97e7802f99e
Tirtha-bit <tirthadeb88@gmail.com>
2026-01-12 22:45:12 +0530

    Add Swagger JWT authorize support

---
+++ src/main/java/com/tirtha/attendance_backend/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.tirtha.attendance_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Attendance Management API",
+                version = "1.0",
+                description = "JWT secured Attendance Backend"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
+
